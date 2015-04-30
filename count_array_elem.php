545468baa6e822299af39657feba7c93aaee880f
Cristina Mani <misha.mani@yahoo.com>
2015-04-30 06:03:41 +0200

    adding elements in array and counting them

---
+++ count_array_elem.php
@@ -0,0 +1,17 @@
+<html>
+    <p>
+	<?php
+	// Create an array and push 5 elements on to it, then 
+    // print the number of elements in your array to the screen
+    $happy = array();
+    array_push($happy, "programming");
+    array_push($happy, "chocolate");
+    array_push($happy, "Coca-Cola");
+    array_push($happy, "hutza-hutza cu carutza");
+    array_push($happy, "Starcraft");
+    
+    print count($happy);
+    
+	?>
+	</p>
+</html>
