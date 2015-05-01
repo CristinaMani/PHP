0a9adb4d840468d4c249c8f7cbade199731b2fe3
Cristina Mani <misha.mani@yahoo.com>
2015-05-02 01:22:32 +0200

    iterating through assoc  arrays

---
+++ iterating_array.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <title>Iteration Nation</title>
+  </head>
+  <body>
+    <p>
+      <?php    
+        $food = array('pizza', 'salad', 'burger');
+        $salad = array('lettuce' => 'with',
+                   'tomato' => 'without',
+                   'onions' => 'with');
+    
+      // Looping through an array using "for".
+      // First, let's get the length of the array!
+      $length = count($food);
+    
+      // Remember, arrays in PHP are zero-based:
+      for ($i = 0; $i < $length; $i++) {
+        echo $food[$i] . '<br />';
+      }
+    
+      echo '<br /><br />I want my salad:<br />';
+    
+      // Loop through an associative array using "foreach":
+      foreach ($salad as $ingredient=>$include) {
+        echo $include . ' ' . $ingredient . '<br />';
+      }
+    
+      echo '<br /><br />';
+    
+      // Create your own array here and loop
+      // through it using foreach!
+    $TVseries = array('GreysAnatomy' => "good",
+                    'Elementary' => 'good',
+                    'VampireDiaries' => 'bad',
+                    'TheMentalist' => 'good');
+                    
+foreach ($TVseries as $name=>$quality) {
+    echo $name." is ".$quality; echo "<br/>";
+}
+                    
+                    
+
+      ?>
+    </p>
+  </body>
+</html>
